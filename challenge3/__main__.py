9e27bdd6e6a4609987c041b67360b352dfe262c8
nkpremices <premices.tuvere@gmail.com>
2020-03-08 23:19:23 +0200

    Start challenge 3

---
+++ challenge3/__main__.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+
+def serialize():
+    pass
+
+
+def deserialize():
+    pass
