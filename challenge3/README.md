d08d75585973f137d0de41eb6b85a74dda107461
nkpremices <premices.tuvere@gmail.com>
2020-03-11 12:06:22 +0200

    Finished challenge 3

--- challenge3/README.md
+++ challenge3/README.md
@@ -23,3 +23,11 @@ assert deserialize(serialize(node)).left.left.val == 'left.left'
 ```
 ### Solution
 
+The secret here is to define the data structure that you are going to use when serializing. In this demo, I will use dictionaries
+
+* create a method called `__str__` in the Node class and make sure it uses recursion in the sense that what it returns calls also the `__str__` method on the members of the class
+* Use the json module to stringify the objects of the class. This will ensure an easy loading when deserializing
+
+* create the serialize method and implement the exact same thing implemented in the `__str__` method. This will ensure the recursion
+* Create the deserialize method. make sure that it calls the `loads` method of `json` and that for each child of the dictionary it checks the type and calls itself in case the type is a dictionary
+ 
--- challenge3/__main__.py
+++ challenge3/__main__.py
@@ -1,14 +1,47 @@
+import json
+import re
+
+
 class Node:
-    def __init__(self, val, left=None, right=None):
+    def __init__(self, val=None, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 
+    def __str__(self):
+        return json.dumps({"val": self.val.__str__(), "left": self.left.__str__(), "right": self.right.__str__()})
 
 
-def serialize():
-    pass
+def deserialize(data):
+    str_data = json.loads(data)
+    dump_node = None
+
+    if str_data["val"]:
+        dump_node = Node(str_data["val"])
+        if str_data["left"]:
+            if str_data["left"] != 'None' and re.match(r'^-?\d+(?:\.\d+)?$', str_data["left"]) is None:
+                dump_node.left = deserialize(str_data["left"])
+            elif re.match(r'^-?\d+(?:\.\d+)?$', str_data["left"]) is not None:
+                dump_node.left = float(str_data["left"])
+            else:
+                dump_node.left = None
+
+        if str_data["right"]:
+            if str_data["right"] != 'None' and re.match(r'^-?\d+(?:\.\d+)?$', str_data["right"]) is None:
+                dump_node.right = deserialize(str_data["right"])
+            elif re.match(r'^-?\d+(?:\.\d+)?$', str_data["right"]) is not None:
+                dump_node.right = float(str_data["right"])
+            else:
+                dump_node.right = None
+
+    return dump_node
 
 
-def deserialize():
-    pass
+def serialize(data):
+    return json.dumps({"val": data.val.__str__(), "left": data.left.__str__(), "right": data.right.__str__()})
+
+
+if __name__ == "__main__":
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+    assert deserialize(serialize(node)).left.left.val == 'left.left'
+    print('----end----')
